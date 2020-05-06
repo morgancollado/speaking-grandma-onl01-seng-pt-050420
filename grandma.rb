eda0e8f7716f7acc47af04e9589e72e7cbd86aeb
Morgan Collado <morgan.collado@gmail.com>
2020-05-06 16:19:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == "Hi".downcase
     return "HUH?! SPEAK UP SONNY!"
+  elsif phrase == "Have you eaten today?".upcase
+  return "NO, NOT SINCE 1983!"
+else phrase == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
 end 
