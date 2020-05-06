9b7722a4cc869745f1365b7b7437bc51352df70c
Morgan Collado <morgan.collado@gmail.com>
2020-05-06 16:21:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase = "hi")
+def speak_to_grandma
   if phrase == "Hi".downcase
     return "HUH?! SPEAK UP SONNY!"
   elsif phrase == "Have you eaten today?".upcase
